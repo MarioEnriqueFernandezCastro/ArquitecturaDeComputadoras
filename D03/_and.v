05de94f5821adc83387aedd5c3db6e65f24e90e4
MarioEnriqueFernandezCastro <mario.fcastro@alumnos.udg.mx>
2025-08-20 22:22:33 -0600

    Create _and.v

---
+++ D03/_and.v
@@ -0,0 +1,12 @@
+//1.Definicion del modulo y sus I/O
+//Dentro del parentesis se define los I/O
+module _and( input a, input b, output c);
+//2.Definen cables o componentes internos
+//NA
+//3.Asignaciones, instancians, conexiones
+
+assign c = a & b; 
+
+
+
+endmodule
